b25a0460348398b53436c7ae0f7bacb914f0fdd8
BilousPM <111942988+BilousPM@users.noreply.github.com>
2022-12-27 11:51:36 +0000

    Deploying to gh-pages from @ BilousPM/upgrade-team@b09800913e8fc7e61ef4c40c66465a7e09e72cf3 🚀

--- index.fb3eb990.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,centeredSlide:!0,effect:"fade",pagination:{el:".swiper-pagination"},grabCursor:!0,navigation:{enabled:!1},mousewheel:{invert:!0,sensitivity:1,eventsTarget:".image-slider__slide",ReleaseOnEdges:!0},autoplay:{delay:13e3,stopOnLastSlide:!1,disableOnInteraction:!1},speed:4500});
-//# sourceMappingURL=index.fb3eb990.js.map
